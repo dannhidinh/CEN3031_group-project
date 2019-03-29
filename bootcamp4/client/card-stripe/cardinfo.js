8cdf4d48af34f9f5c2b598d57bf94afd8e0f857d
hoangdannhi <dinh.n@ufl.edu>
2019-03-29 15:06:01 -0400

    more stripe stuff

--- bootcamp4/client/card-stripe/cardinfo.js
+++ bootcamp4/client/card-stripe/cardinfo.js
@@ -1,52 +1,25 @@
-var stripe = require("stripe")("sk_test_5szqSVkKfzjcxPvMPFSBLMxB00iWi4Ie9i");
-stripe.charges.retrieve("ch_1EIzdLJ6O0ymFSXqbBEguCj0", {
-  api_key: "sk_test_5szqSVkKfzjcxPvMPFSBLMxB00iWi4Ie9i",
-  expand: ["customer"],
-});
-stripe.charges.create({
-  amount: 999,
-  currency: "usd",
-  source: "tok_amex",
-  description: "Example charge"
-  },{
-    idempotency_key: "AKBlM0uKaTZCCsPC"
-  }, function(err, charge) {
-    if err throw err;
-    // asynchronously called
+var handler = Stripe.configure({
+  key: 'pk_test_iTugFek1yZMY2i7fqgtKnauz00RFrdnY7a',
+  image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+  locale: 'auto',
+  token: function(token) {
+    // You can access the token ID with `token.id`.
+    // Get the token ID to your server-side code for use.
+  }
 });
 
-/*
-const token = request.body.stripeToken;
-(async () => {
-  const charge = await stripe.charges.create({
-    amount: 999,
-    currency: 'usd',
-    description: 'Test charge',
-    source: token,
-    statement_descriptor: 'Custom descriptor',
+// connect this to some backend stuff
+document.getElementById('customButton').addEventListener('click', function(e) {
+  // Open Checkout with further options:
+  handler.open({
+    name: 'Purchase',
+    description: 'Checkout Cart',
+    amount: 2000 //change, connect to database or smth
   });
-})();
-*/
+  e.preventDefault();
+});
 
-// failed charges, error handling - EDIT
-var message = "Error: Your card has been declined";
-switch (err.type) {
-  case 'StripeCardError':
-    err.message;
-    break;
-  case 'RateLimitError':
-    // invalid parameters
-    break;
-  case 'StripeAPIError':
-    // internal error with Stripe's API
-    break;
-  case 'StripeConnectionError':
-    // error with https communication
-    break;
-  case 'StripeAuthenticationError':
-    // used incorrect API key
-    break:
-  default:
-    // other unexpected erorrs;
-    break;
-}
+// Close Checkout on page navigation:
+window.addEventListener('popstate', function() {
+  handler.close();
+});
